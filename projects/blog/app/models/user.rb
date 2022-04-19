5b590bd3a8f8627a51d15dd5608cba3782be850c
Brandon <brandondeval+github@gmail.com>
2022-04-19 12:33:32 -0600

    add bcrypt

--- projects/blog/app/models/user.rb
+++ projects/blog/app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
+	before_save { self.email = email.downcase }
 	has_many :tools
 	has_many :notes
 	validates :firstName, length: { maximum: 40 }, presence: true
 	validates :middleName, length: { maximum: 40 }, presence: true
 	validates :lastName, length: { maximum: 40 }, presence: true
 	validates :phone, length: { maximum: 40 }, presence: true
-	validates :email, length: { maximum: 40 }, presence: true
+	validates :email, presence: true, length: { maximum: 40 }, uniqueness: true
+	has_secure_password
 end
--- projects/blog/db/schema.rb
+++ projects/blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_17_014428) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_19_182801) do
   create_table "notes", force: :cascade do |t|
     t.text "content"
     t.integer "tool_id"
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_17_014428) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
